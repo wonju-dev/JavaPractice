b5a037057d63868b50c63c5e3c4c0fb59e91ec9d
wonju-dev <wonjulee.dev@gmail.com>
2022-01-07 14:03:40 +0900

    type casting in reference type

--- src/main.java
+++ src/main.java
@@ -1,11 +1,8 @@
 public class main {
     public static void main(String args[]) {
-        User myUser = new User("wonju", 25, 0);
-        SuperUser mySuperUser = new SuperUser("wonju2", 20, 1, new int[] {1,2,3,4});
-        User polyUser = new SuperUser("wonju3", 15, 0, new int[]{5,6,7,8});
-
-        System.out.println(myUser.getAge());
-        System.out.println(mySuperUser.getprivileges());
-        // System.out.println(polyUser.getprivileges());
+        SuperUser superUser = new SuperUser("a",1,2,new int[]{1,2,3});
+        User user = superUser;
+        SuperUser newSuperUser = (SuperUser) user;
+        System.out.println(newSuperUser.getprivileges());
     }
 }
