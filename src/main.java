fe86898d101a87e664e0deab6674edbf3e4f9fa7
wonju-dev <wonjulee.dev@gmail.com>
2022-01-10 15:28:58 +0900

    exception handling - throw Exception

--- src/main.java
+++ src/main.java
@@ -1,15 +1,15 @@
-import java.lang.ArrayIndexOutOfBoundsException;
-
 public class main {
     public static void main(String args[]) {
-        try {
-            int[] intArray = new int[] {1,2,3,4,5};
-            for (int i = 0 ; i < intArray.length + 1 ; i++) {
+        int[] intArray = new int[]{0, 1, 2, 3, 4};
+        for (int i = 0; i < intArray.length + 1; i++) {
+            try {
+                if (i == 1) throw new Exception("index is 1");
                 System.out.println(intArray[i]);
+            } catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("index out of bound");
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
-        } catch (Exception ArrayIndexOutOfBoundsException) {
-            System.out.println("index out of bound");
         }
-        System.out.println("but program has finished with on Error");
     }
 }
