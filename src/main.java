4e39eea6256f7be98b2ff6136d7e3c926c57171e
wonju-dev <wonjulee.dev@gmail.com>
2022-01-10 15:49:48 +0900

    finally

--- src/main.java
+++ src/main.java
@@ -1,17 +1,15 @@
 public class main {
     public static void main(String args[]) {
         try {
-            myMethod1();
-        } catch (Exception e) {
-            e.printStackTrace();
+            myMethod();
+        } catch(ArrayIndexOutOfBoundsException e) {
+            e.getMessage();
+        } finally {
+            System.out.println("finally");
         }
     }
 
-    static void myMethod1() throws Exception{
-        myMethod2();
-    }
-
-    static void myMethod2() throws Exception {
+    static void myMethod() throws ArrayIndexOutOfBoundsException {
         int[] intArray = new int[]{0, 1, 2, 3, 4};
         for (int i = 0; i < intArray.length + 1; i++) {
             System.out.println(intArray[i]);
