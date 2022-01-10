dba0d899a9843f65590dd6d00523eb0f9c4c07f8
wonju-dev <wonjulee.dev@gmail.com>
2022-01-10 18:48:52 +0900

    pratice

--- src/Computer.java
+++ src/Computer.java
@@ -1,9 +1,12 @@
 public class Computer {
-    static int a;
-    static {
-        a = 10;
-    }
+    int b = 111;
+    static int a = 10;
     Computer(int a){
         this.a = a;
+    }
+    void print() {
+        System.out.println(this);
+        System.out.println("hi");
+        System.out.println(a);
     }
 }
--- src/main.java
+++ src/main.java
@@ -3,14 +3,7 @@ import java.lang.Integer;
 
 public class main {
     public static void main(String args[]) {
-        System.out.println(Computer.a);
-        Computer myComputer = new Computer(1);
-        System.out.println(myComputer.a);
-        System.out.println(Computer.a);
-        System.out.println(returnFloat(10));
-    }
-
-    static float returnFloat(int a) {
-        return a; // typecasting
+        Computer myComputer = new Computer(10);
+        myComputer.print();
     }
 }
