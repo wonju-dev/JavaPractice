d64dccec5cc4429e71971e093bcde5caf0ac55ea
wonju-dev <wonjulee.dev@gmail.com>
2022-01-10 15:40:45 +0900

    multi level method throws

--- src/main.java
+++ src/main.java
@@ -1,18 +1,19 @@
 public class main {
     public static void main(String args[]) {
         try {
-            myMethod();
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println(e.getMessage());
+            myMethod1();
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
     }
 
-    static void myMethod() throws Exception {
+    static void myMethod1() throws Exception{
+        myMethod2();
+    }
+
+    static void myMethod2() throws Exception {
         int[] intArray = new int[]{0, 1, 2, 3, 4};
         for (int i = 0; i < intArray.length + 1; i++) {
-            if (i == 1) throw new Exception("index is 1");
             System.out.println(intArray[i]);
         }
     }
