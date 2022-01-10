ef4866bcf8bf2b34c7d0f47c32972638e0956d4e
wonju-dev <wonjulee.dev@gmail.com>
2022-01-10 15:36:57 +0900

    exception handling : method throws expections

--- src/main.java
+++ src/main.java
@@ -1,15 +1,19 @@
 public class main {
     public static void main(String args[]) {
+        try {
+            myMethod();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    static void myMethod() throws Exception {
         int[] intArray = new int[]{0, 1, 2, 3, 4};
         for (int i = 0; i < intArray.length + 1; i++) {
-            try {
-                if (i == 1) throw new Exception("index is 1");
-                System.out.println(intArray[i]);
-            } catch (ArrayIndexOutOfBoundsException e){
-                System.out.println("index out of bound");
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
+            if (i == 1) throw new Exception("index is 1");
+            System.out.println(intArray[i]);
         }
     }
 }
