32168a7b61243004c538f63bdad506ca3fc69277
wonju-dev <wonjulee.dev@gmail.com>
2022-01-11 15:29:20 +0900

    java.lang.object clone overriding deep copy

--- src/Circle.java
+++ src/Circle.java
@@ -4,11 +4,21 @@ public class Circle implements Cloneable{
         this.point = point;
     }
 
-    public Circle clone() {
+    public Circle shallowClone() {
         Object newCicle = null;
         try{
             newCicle = super.clone();
         } catch (CloneNotSupportedException e) {}
         return (Circle) newCicle;
+    }
+
+    public Circle deepClone(){
+        Object obj = null;
+        try{
+            obj = super.clone();
+        } catch (CloneNotSupportedException e) {}
+        Circle newCircle = (Circle) obj;
+        newCircle.point = new Point(this.point.x, this.point.y);
+        return newCircle;
     }
 }
--- src/main.java
+++ src/main.java
@@ -4,13 +4,14 @@ import java.lang.Integer;
 public class main {
     public static void main(String args[]) {
         Circle c1 = new Circle(new Point(1,2));
-        Circle c2 = c1.clone();
+        Circle c2 = c1.deepClone(); // deep copy
         Circle c3 = c1; // Shallow copy
         System.out.println(c1.point.toString());
         System.out.println(c2.point.toString());
         System.out.println(c3.point.toString());
 
         c1.point.x = 100;
+        
         System.out.println(c1.point.toString());
         System.out.println(c2.point.toString());
         System.out.println(c3.point.toString());
