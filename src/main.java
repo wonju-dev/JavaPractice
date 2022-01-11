249ad1ed68720da3292870bb67cf8b9b3d9bea91
wonju-dev <wonjulee.dev@gmail.com>
2022-01-11 16:19:53 +0900

    String literal vs Constructor

--- src/main.java
+++ src/main.java
@@ -3,18 +3,14 @@ import java.lang.Integer;
 
 public class main {
     public static void main(String args[]) {
-        Circle c1 = new Circle(new Point(1,2));
-        Circle c2 = c1.deepClone(); // deep copy
-        Circle c3 = c1; // Shallow copy
-        System.out.println(c1.point.toString());
-        System.out.println(c2.point.toString());
-        System.out.println(c3.point.toString());
-
-        c1.point.x = 100;
-        
-        System.out.println(c1.point.toString());
-        System.out.println(c2.point.toString());
-        System.out.println(c3.point.toString());
-
+        String a = "abc";
+        String b = "abc";
+        String c = new String("abc");
+        String d = new String("abc");
+        System.out.println(a == b);
+        System.out.println(c == d);
+        System.out.println(a.equals(b));
+        System.out.println(b.equals(c));
+        System.out.println(c.equals(d));
     }
 }
