d4b520a151f7231a81b9c5a7f8182714b8e4452e
wonju-dev <wonjulee.dev@gmail.com>
2022-01-12 17:03:34 +0900

    absract enum method

--- src/main.java
+++ src/main.java
@@ -1,14 +1,25 @@
 public class main {
 
     public static void main(String args[]) {
-        System.out.println(Gender.MALE.getValue());
-        System.out.println(Gender.FEMALE.getValue());
+        System.out.println(Gender.MALE.getExpectedLife(10));
+        System.out.println(Gender.FEMALE.getExpectedLife(12));
     }
 }
 enum Gender {
-    MALE(10), FEMALE(2);
+    MALE(10){
+        @Override
+        int getExpectedLife(int workload) {
+            return 2 * this.getBASE_LIFE_EXPECTANCY() * workload;
+        }
+    }, FEMALE(2) {
+        @Override
+        int getExpectedLife(int workload) {
+            return 3 * this.getBASE_LIFE_EXPECTANCY() * workload;
+        }
+    };
 
-    private final int value;
-    Gender(int value) {this.value = value;}
-    public int getValue() { return this.value; }
+    private final int BASE_LIFE_EXPECTANCY;
+    Gender(int value) {this.BASE_LIFE_EXPECTANCY = value;}
+    public int getBASE_LIFE_EXPECTANCY() { return this.BASE_LIFE_EXPECTANCY; }
+    abstract int getExpectedLife(int workload);
 }
