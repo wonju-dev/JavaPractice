1797543f3a93d476531f55966e6d0bd1938dc6c6
wonju-dev <wonjulee.dev@gmail.com>
2022-01-16 00:14:48 +0900

    TreeSet subSet, headSet, tailSet

--- src/main.java
+++ src/main.java
@@ -1,14 +1,17 @@
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.TreeSet;
 
 public class main {
 
     public static void main(String args[]) {
         TreeSet ts = new TreeSet();
-        ts.add(5);
-        ts.add(1);
-        ts.add(4);
-        ts.add(8);
-        ts.add(2);
-        System.out.println(ts);
+        Integer[] array = {1,5,2,4,3};
+        for (Integer integer : array) ts.add(integer);
+
+        System.out.println(ts.subSet(2,4));
+        System.out.println(ts.headSet(3));
+        System.out.println(ts.tailSet(3));
+
     }
 }
