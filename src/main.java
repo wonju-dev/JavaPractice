1a61d378ddc4ccd8a7c6be8b88a13cc39bcb5e4d
wonju-dev <wonjulee.dev@gmail.com>
2022-01-15 23:11:15 +0900

    HashSet add (equals, hashCode)

--- src/main.java
+++ src/main.java
@@ -3,14 +3,45 @@ import java.util.*;
 public class main {
 
     public static void main(String args[]) {
-        Set hs = new HashSet();
-        Set lhs = new LinkedHashSet();
+        HashSet set = new HashSet();
+        set.add("abc");
+        set.add("abc");
+        set.add(new Person("a", 1));
+        set.add(new Person("a", 1));
+        set.add(new Person2("b", 2));
+        set.add(new Person2("b", 2));
+        System.out.print(set); // [abc, Person, Person, Person2]
+    }
+}
 
-        List l = new LinkedList(hs); // linekdlist는 collection을 구현하는 class는 전부 가능한 듯?
-        Collections.sort(l); // Hashset 정렬을 위해 linkedlist로 감싸준다
-        Iterator it = l.iterator();
-        while (it.hasNext()) {
-            System.out.println(it.next());
-        }
+class Person { // Person Class에 equals(), hashCode()가 없으므로, Super Class (Object)의 method를 사용한다
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class Person2 {
+    String name;
+    int age;
+
+    Person2(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Person2)) return false;
+
+        Person2 p2 = (Person2) obj;
+        return this.name.equals(p2.name) && this.age == p2.age;
+    }
+
+    public int hashCode() {
+        return Objects.hash(name + age); // JDK 1.8에 추가된 Object class의 hash함수를 이용해서 새로운 hashCode를 생성할 수 있다
+        // return (name+age).hashCode(); // JDK 1.8 이전버전, 되도록 위의 방법을 사용하자
     }
 }
