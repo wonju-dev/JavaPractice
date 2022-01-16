00834b4d0a647a13391594266743da17448eda12
wonju-dev <wonjulee.dev@gmail.com>
2022-01-16 18:27:29 +0900

    HashMap +

--- src/main.java
+++ src/main.java
@@ -1,17 +1,30 @@
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 public class main {
 
     public static void main(String args[]) {
         HashMap<String, Object> hm = new HashMap<String, Object>();
         hm.put("key1", new Integer(1)); // hm.put("1", 1);
+        hm.put("key2", new Integer(2));
 
         Set entries = hm.entrySet();
+        System.out.println(entries);
         Iterator it = entries.iterator();
         while (it.hasNext()) {
             System.out.println(it.next());
+            System.out.println(it.next() instanceof Map.Entry);
+        }
+
+        Set keySet = hm.keySet(); // key를 set으로 반환
+        Iterator keySetIT = keySet.iterator();
+        while (keySetIT.hasNext()) {
+            System.out.println(keySetIT.next());
+        }
+
+        Collection values = hm.values();
+        Iterator valueIt = values.iterator();
+        while (valueIt.hasNext()){
+            System.out.println(valueIt.next());
         }
 
         System.out.println(hm.containsKey("1"));
