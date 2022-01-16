15cc0df0c6c41ce05384feac7e7ea4423c1125b0
wonju-dev <wonjulee.dev@gmail.com>
2022-01-16 18:18:34 +0900

    HashMap

--- src/main.java
+++ src/main.java
@@ -1,17 +1,25 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.TreeSet;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 
 public class main {
 
     public static void main(String args[]) {
-        TreeSet ts = new TreeSet();
-        Integer[] array = {1,5,2,4,3};
-        for (Integer integer : array) ts.add(integer);
+        HashMap<String, Object> hm = new HashMap<String, Object>();
+        hm.put("key1", new Integer(1)); // hm.put("1", 1);
 
-        System.out.println(ts.subSet(2,4));
-        System.out.println(ts.headSet(3));
-        System.out.println(ts.tailSet(3));
+        Set entries = hm.entrySet();
+        Iterator it = entries.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
 
+        System.out.println(hm.containsKey("1"));
+        System.out.println(hm.containsValue(1));
+
+        HashMap hm2 = new HashMap();
+        hm2.put("1", 1);
+        hm2.put('2',2);
+        System.out.println(hm2.containsKey("2"));
     }
 }
