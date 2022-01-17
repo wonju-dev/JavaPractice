4e0fa47f07d2b7a5729c980a99f65215efdb16af
wonju-dev <wonjulee.dev@gmail.com>
2022-01-17 15:10:46 +0900

    TreeMap

--- src/main.java
+++ src/main.java
@@ -2,25 +2,24 @@ import java.util.*;
 
 public class main {
     public static void main(String args[]) {
-        PhoneBook.addNewPhoneNumber("친구", new Person("친구1", "1000"));
-        PhoneBook.addNewPhoneNumber("친구", new Person("친구2", "1001"));
-        PhoneBook.addNewPhoneNumber("친구", new Person("친구3", "1002"));
+        String[] data = {"a","b","c","d","a","c","d","a","c","a"};
+        TreeMap tp = new TreeMap();
 
-        PhoneBook.addNewPhoneNumber("가족", new Person("가족1", "2000"));
-        PhoneBook.addNewPhoneNumber("가족", new Person("가족2", "2001"));
-        PhoneBook.addNewPhoneNumber("가족", new Person("가족3", "2002"));
-        HashMap friendList = PhoneBook.getCategory("친구");
-        HashMap coworkerList = PhoneBook.getCategory("동료");
-        System.out.println(friendList);
-        System.out.println(coworkerList);
-
-        try {
-            String number1 = PhoneBook.findWithName("친구1");
-            System.out.printf("%s %s \n", "친구1", number1);
-            String number2 = PhoneBook.findWithName("깐부1");
-            System.out.printf("%s %s", "깐부1", number2);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        for (int i = 0 ; i < data.length ; i++){
+            if (tp.containsKey(data[i])) {
+                int value = (int) tp.get(data[i]);
+                tp.put(data[i], value + 1);
+                System.out.printf("%s %d \n",data[i], value);
+            } else {
+                tp.put(data[i], 1);
+            }
         }
-    }
+
+        Iterator it = tp.values().iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+        System.out.println(tp.get("a"));
+
+   }
 }
