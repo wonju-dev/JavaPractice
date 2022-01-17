5b75eeeb0565dc2c1c7557513e3793608151a8b4
wonju-dev <wonjulee.dev@gmail.com>
2022-01-17 15:18:26 +0900

    Properties

--- src/main.java
+++ src/main.java
@@ -2,24 +2,17 @@ import java.util.*;
 
 public class main {
     public static void main(String args[]) {
-        String[] data = {"a","b","c","d","a","c","d","a","c","a"};
-        TreeMap tp = new TreeMap();
+        Properties properties = new Properties();
+        properties.setProperty("pro1", "value1");
+        properties.setProperty("pro2", "value2");
+        properties.setProperty("pro3", "value3");
 
-        for (int i = 0 ; i < data.length ; i++){
-            if (tp.containsKey(data[i])) {
-                int value = (int) tp.get(data[i]);
-                tp.put(data[i], value + 1);
-                System.out.printf("%s %d \n",data[i], value);
-            } else {
-                tp.put(data[i], 1);
-            }
+        Enumeration e = properties.propertyNames();
+
+        while (e.hasMoreElements()) {
+            String element = (String) e.nextElement();
+            System.out.printf("%s %s \n", element, properties.getProperty(element));
         }
 
-        Iterator it = tp.values().iterator();
-        while (it.hasNext()) {
-            System.out.println(it.next());
-        }
-        System.out.println(tp.get("a"));
-
-   }
+    }
 }
