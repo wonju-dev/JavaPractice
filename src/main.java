c49c87717114dfdfa60c2572ec90a11754858488
wonju-dev <wonjulee.dev@gmail.com>
2022-01-18 18:45:32 +0900

    Collection framework functional interface

--- src/main.java
+++ src/main.java
@@ -1,39 +1,18 @@
-import java.util.function.Predicate;
-import java.util.function.Supplier;
-import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
-import java.lang.Runnable;
+import java.util.ArrayList;
+import java.util.function.UnaryOperator;
 
 public class main {
     public static void main(String args[]) {
-        Predicate<String> isEmptyStr = s -> s.length() == 0;
-        System.out.println(isEmptyStr.test("s"));
-        System.out.println(isEmptyStr.test(""));
+        ArrayList<Person> memberList = new ArrayList<Person>();
+        for (int i = 0 ; i < 10 ; i++) memberList.add(new Person("name"+i ,i));
 
-        Runnable printHi = () -> System.out.println("hi");
-        printHi.run();
-
-        Supplier<String> getWelcomeMessage = () -> "welcome!";
-        String welcomeMessage = getWelcomeMessage.get();
-        System.out.println(welcomeMessage);
-
-        Supplier<Person> getNewPerson = () -> new Person("no name", 0);
-        Person p1 = getNewPerson.get();
-        System.out.println(p1);
-
-        BiConsumer<Person, String> setName = (Person p, String newName) -> p.name = newName;
-        setName.accept(p1, "new name");
-        System.out.println(p1);
-
-        BiFunction<Person, Integer, Integer>  setAndGetAge = (Person p, Integer newAge) -> {
-            int oldAge = p.age;
-            p.age = newAge;
-            return oldAge;
-        };
-        int oldAge = setAndGetAge.apply(p1, 10);
-        System.out.println(p1);
-        System.out.println(oldAge);
-
+        memberList.replaceAll((Person p)-> {
+            String nameIndex = p.name.substring(4);
+            if (Integer.parseInt(nameIndex) % 2 == 0) p.name = p.name + nameIndex;
+            else p.name = p.name + nameIndex + nameIndex;
+            return p;
+        });
+        memberList.forEach((Person p)->System.out.println(p));
     }
 }
 
@@ -47,6 +26,6 @@ class Person {
     }
 
     public String toString(){
-        return "name : '" + this.name + "'age : " + this.age;
+        return "name : '" + this.name + "' age : " + this.age;
     }
 }
