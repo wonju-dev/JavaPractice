1bc69843b9b6cf71676db8041b6d0d2992d883ef
wonju-dev <wonjulee.dev@gmail.com>
2022-01-18 15:13:32 +0900

    lambda as Return Type

--- src/main.java
+++ src/main.java
@@ -4,11 +4,19 @@ public class main {
     public static void main(String args[]) {
         MyMethod f = () -> System.out.println("lambda");
         aMethod(f);
+
+        MyMethod ff = bMethod();
+        ff.max();
     }
 
     static void aMethod(MyMethod f) {
         System.out.println("hi");
         f.max();
+    }
+
+    static MyMethod bMethod() {
+        MyMethod f = () -> System.out.println("new Lambda");
+        return f;
     }
 }
 
