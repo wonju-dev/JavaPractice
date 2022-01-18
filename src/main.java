5e7366a31d7d72ed7c75b04132677ce5aa0a9a0a
wonju-dev <wonjulee.dev@gmail.com>
2022-01-18 17:54:39 +0900

    java.lang.Runnable run

--- src/main.java
+++ src/main.java
@@ -1,10 +1,16 @@
 import java.util.function.Predicate;
+import java.lang.Runnable;
 
 public class main {
     public static void main(String args[]) {
         Predicate<String> isEmptyStr = s -> s.length() == 0;
         System.out.println(isEmptyStr.test("s"));
         System.out.println(isEmptyStr.test(""));
+
+        Runnable printHi = () -> System.out.println("hi");
+        printHi.run();
+
+
     }
 }
 
