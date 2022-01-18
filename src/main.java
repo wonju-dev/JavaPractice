b3ce6f7f5ea863ccbcd99059be24f3ac680cdf76
wonju-dev <wonjulee.dev@gmail.com>
2022-01-18 20:04:22 +0900

    Functional Interface Combination

--- src/main.java
+++ src/main.java
@@ -1,48 +1,14 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.function.UnaryOperator;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class main {
     public static void main(String args[]) {
-        ArrayList<Person> memberList = new ArrayList<Person>();
-        Map<String, String > phoneNumber = new HashMap<String, String>();
+        Function<String, Integer> f = (s) -> Integer.parseInt(s); // String -> int
+        Function<Integer, String> g = (s) -> Integer.toBinaryString(s); // 10 -> 2
+        Function<String, String> h = f.andThen(g);
+        System.out.println(h.apply("10"));
+        Function<Integer, Integer> h2 = f.compose(g);
+        System.out.println(h2.apply(10));
 
-        for (int i = 0 ; i < 10 ; i++) {
-            String name = "name"+i;
-            Person newPerson = new Person(name, i);
-            memberList.add(newPerson);
-            phoneNumber.put(name, ""+i+i+i);
-        }
-
-        memberList.replaceAll((Person p)-> {
-            String nameIndex = p.name.substring(4);
-            if (Integer.parseInt(nameIndex) % 2 == 0) p.name = p.name + nameIndex;
-            else p.name = p.name + nameIndex + nameIndex;
-            return p;
-        });
-
-        memberList.forEach((Person p)->System.out.println(p));
-        Iterator it = phoneNumber.entrySet().iterator();
-        while (it.hasNext()) {
-            System.out.println(it.next());
-        }
-
-
-    }
-}
-
-class Person {
-    String name;
-    int age;
-
-    Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public String toString(){
-        return "name : '" + this.name + "' age : " + this.age;
     }
 }
