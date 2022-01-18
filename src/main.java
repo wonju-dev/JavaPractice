c8a9696b614c9b4fc6e52626839ac23c04dc5e2f
wonju-dev <wonjulee.dev@gmail.com>
2022-01-18 19:47:20 +0900

    Collection framework functional interface Map

--- src/main.java
+++ src/main.java
@@ -1,10 +1,20 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.function.UnaryOperator;
 
 public class main {
     public static void main(String args[]) {
         ArrayList<Person> memberList = new ArrayList<Person>();
-        for (int i = 0 ; i < 10 ; i++) memberList.add(new Person("name"+i ,i));
+        Map<String, String > phoneNumber = new HashMap<String, String>();
+
+        for (int i = 0 ; i < 10 ; i++) {
+            String name = "name"+i;
+            Person newPerson = new Person(name, i);
+            memberList.add(newPerson);
+            phoneNumber.put(name, ""+i+i+i);
+        }
 
         memberList.replaceAll((Person p)-> {
             String nameIndex = p.name.substring(4);
@@ -12,7 +22,14 @@ public class main {
             else p.name = p.name + nameIndex + nameIndex;
             return p;
         });
+
         memberList.forEach((Person p)->System.out.println(p));
+        Iterator it = phoneNumber.entrySet().iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+
+
     }
 }
 
