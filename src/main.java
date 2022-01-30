a5e0f58af0902e07c9882dcfeb109f8b4ba066fd
wonju-dev <wonjulee.dev@gmail.com>
2022-01-30 16:23:40 +0900

    Fix : Arrays.sort needs for Comparable, Comaparator

--- src/main.java
+++ src/main.java
@@ -6,18 +6,33 @@ public class main {
         MyInteger[] myIntegers = new MyInteger[10];
 
         for (int i = 0 ; i < 10 ; i++) {
-            myIntegers[i] = new MyInteger((int) Math.random() * 100);
+            myIntegers[i] = new MyInteger((int) (Math.random() * 100));
         }
 
-        Arrays.sort(myIntegers); // Error : has no comparator, comparable 
+        Arrays.sort(myIntegers);
+
+        for (MyInteger numbers : myIntegers) {
+            System.out.println(numbers);
+        }
 
     }
 }
 
-class MyInteger {
+class MyInteger implements Comparable<MyInteger>{
     private int myValue;
 
     public MyInteger(int myValue) {
         this.myValue = myValue;
+    }
+
+    @Override
+    public int compareTo(MyInteger integer) {
+        if (this.myValue > integer.myValue) return 1;
+        else if (this.myValue == integer.myValue) return 0;
+        else return -1;
+    }
+
+    public String toString() {
+        return this.myValue + "";
     }
 }
