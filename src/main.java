d9464946770445255555f6da7bb08f4c1737eeb9
wonju-dev <wonjulee.dev@gmail.com>
2022-02-02 17:25:02 +0900

    Stream with Random Class

--- src/main.java
+++ src/main.java
@@ -1,17 +1,14 @@
+import java.util.Random;
 import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
-import java.util.stream.LongStream;
-import java.util.stream.Stream;
 
 
 public class main {
     public static void main(String[] args) {
-        IntStream intStream = IntStream.range(1, 5); // [1,2,3,4]
-        LongStream longStream = LongStream.rangeClosed(1, 5); // [1, 2, 3, 4, 5]
-        DoubleStream doubleStream = DoubleStream.builder().add(1).add(2).add(3).build(); // [1, 2, 3]
+        DoubleStream doubleStream = new Random().doubles(3);
+        doubleStream.forEach((number) -> System.out.println(number));
 
-        Stream<Integer> intStream2 = IntStream.range(1, 5).boxed();
-        Stream<Long> longStream2 = LongStream.range(1, 5).boxed();
-        Stream<Double> doubleStream2 = DoubleStream.builder().add(1).add(2).add(3).build().boxed();
+        IntStream intStream = new Random().ints(3);
+        intStream.forEach((number) -> System.out.println(number));
     }
 }
