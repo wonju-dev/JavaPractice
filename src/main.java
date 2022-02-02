aadc4da238005fd8deadd36782beade235c06058
wonju-dev <wonjulee.dev@gmail.com>
2022-02-02 14:58:12 +0900

    lambda & Comparator

--- src/main.java
+++ src/main.java
@@ -1,6 +1,4 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class main {
     public static void main(String[] args) {
@@ -16,10 +14,27 @@ public class main {
         };
 
         List students = new ArrayList<Student>();
-        for (int i = 1 ; i <= 5 ; i++) students.add(new Student(i,Integer.toString(i)));
+        for (int i = 1 ; i <= 5 ; i++) {
+            int random = (int) (Math.random() * 100);
+            students.add(new Student(random, Integer.toString(random)));
+        };
 
         Student oldest = f.getOldestStudent(students);
-        System.out.println(oldest);
+        System.out.printf("oldest student is %s \n", oldest);
+
+        Collections.sort(students, new Comparator<Student>() {
+            public int compare(Student s1, Student s2) {
+                if (s1.getAge() > s2.getAge()) return 1;
+                else if (s1.getAge() == s2.getAge()) return 0;
+                else return -1;
+            }
+        });
+
+        System.out.println("after sort");
+        Iterator it = students.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
 
     }
 }
