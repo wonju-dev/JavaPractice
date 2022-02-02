30daf1e49d55c0bd127f5aed9f4411ad77d56b0b
wonju-dev <wonjulee.dev@gmail.com>
2022-02-02 17:26:42 +0900

    Straem concat

--- src/main.java
+++ src/main.java
@@ -1,14 +1,13 @@
-import java.util.Random;
-import java.util.stream.DoubleStream;
-import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 
 public class main {
     public static void main(String[] args) {
-        DoubleStream doubleStream = new Random().doubles(3);
-        doubleStream.forEach((number) -> System.out.println(number));
+        Stream<String> stream1 = Stream.of("a", "b", "C");
+        Stream<String> stream2 = Stream.of("d", "e", "f");
+        Stream<String> stream3 = Stream.concat(stream1, stream2);
 
-        IntStream intStream = new Random().ints(3);
-        intStream.forEach((number) -> System.out.println(number));
+        stream3.forEach((c)-> System.out.println(c));
+
     }
 }
